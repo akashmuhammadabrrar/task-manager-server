bf42dfdb2537aa64a9fb7778d85ab87a702ad2e1
AkashMAbrrar <madborakash48@gmail.com>
2025-02-21 01:23:19 +0600

    ready to deploy

--- index.js
+++ index.js
@@ -1,64 +1,3 @@
-// const express = require("express");
-// const app = express();
-// const cors = require("cors");
-// require("dotenv").config();
-// const port = process.env.PORT || 5001;
-
-// // middlewares
-// app.use(cors());
-// app.use(express.json());
-
-// const { MongoClient, ServerApiVersion } = require("mongodb");
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c4n3e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   },
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     const taskCollection = client.db("toDoDB").collection("tasks");
-
-//     // post task data
-//     app.post("/tasks", async (req, res) => {
-//       const task = req.body;
-//       const result = await taskCollection.insertOne(task);
-//       res.send(result);
-//     });
-
-//     // get task data
-//     app.get("/tasks", async (req, res) => {
-//       const result = await taskCollection.find().toArray();
-//       res.send(result);
-//     });
-
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log(
-//       "Pinged your deployment. You successfully connected to MongoDB!"
-//     );
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
-// app.get("/", (req, res) => {
-//   res.send("Task Is Coming Soon");
-// });
-
-// app.listen(port, () => {
-//   console.log(`Task is running on port ${port}`);
-// });
-
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -87,11 +26,11 @@ let taskCollection;
 // Connect to MongoDB once
 async function connectDB() {
   try {
-    await client.connect();
+    // await client.connect();
     taskCollection = client.db("toDoDB").collection("tasks");
-    console.log("Connected to MongoDB ");
+    // console.log("Connected to MongoDB ");
   } catch (error) {
-    console.error("MongoDB connection error :", error);
+    // console.error("MongoDB connection error :", error);
   }
 }
 
