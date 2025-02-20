13b1f8055008a126f8632f290aa523a00f3049ed
AkashMAbrrar <madborakash48@gmail.com>
2025-02-21 00:07:17 +0600

    this commit for task deleted api

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ const port = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c4n3e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient
@@ -121,6 +121,14 @@ app.get("/tasks", async (req, res) => {
     console.error("Error fetching tasks:", error);
     res.status(500).json({ message: "Internal Server Error", error });
   }
+});
+
+// delete task data
+app.delete("/tasks/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await taskCollection.deleteOne(query);
+  res.send(result);
 });
 
 // Root Route
